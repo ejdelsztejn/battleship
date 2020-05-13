d51e3d37933c64f5eb27f3d5235c714c3f28464f
Dani Coleman <danicoleman00@gmail.com>
2020-05-12 19:39:33 -0600

    Add cell_test.rb

---
+++ test/cell_test.rb
@@ -0,0 +1,41 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+
+class CellTest < Minitest::Test
+
+  def test_it_exists
+    # skip
+    cell = Cell.new("B4")
+
+    assert_instance_of Cell, cell
+  end
+
+  def test_it_has_a_coordinate
+    # skip
+    cell = Cell.new("B4")
+
+    assert_equal "B4", cell.coordinate
+   end
+
+  def test_it_is_empty
+    # skip
+    cell = Cell.new("B4")
+
+    assert_nil nil, cell.ship
+    assert_equal true, cell.empty?
+   end
+
+   def test_it_can_place_ship
+    # skip
+    cell = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    cell.place_ship(cruiser)
+    assert_instance_of Ship, cell.ship
+    assert_equal false, cell.empty?
+   end
+
+
+end
