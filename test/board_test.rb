fd62f5499bb8cbd6a1b5bb2902a73b5c9e679a39
Dani Coleman <danicoleman00@gmail.com>
2020-05-14 08:12:53 -0600

    Add test up to Overlapping Ships portion of iteration 2

--- test/board_test.rb
+++ test/board_test.rb
@@ -71,7 +71,17 @@ class BoardTest < Minitest::Test
     assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
   end
 
-  def method_name
+  def test_it_can_place_ship
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
 
+    board.place(cruiser, ["A1", "A2", "A3"])
+    assert_equal Ship, cell_1.ship
+    assert_equal Ship, cell_2.ship
+    assert_equal Ship, cell_3.ship
+    assert_equal true, cell_3.ship == cell_2.ship
   end
 end
