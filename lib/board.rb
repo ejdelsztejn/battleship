cf7dd1e3c47954d863f27936f962a743b5fc5b1f
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-14 11:33:30 -0400

    Add valid_coordinate? method

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,8 @@ class Board
       "D1" => Cell.new("D1"), "D2" => Cell.new("D2"), "D3" => Cell.new("D3"), "D4" => Cell.new("D4")
     }
   end
+
+  def valid_coordinate?(coordinate)
+    cells.keys.include?(coordinate)
+  end
 end
