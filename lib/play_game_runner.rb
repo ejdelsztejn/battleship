1a74460f302641b3969dc0c162584c74eb23f444
Dani Coleman <danicoleman00@gmail.com>
2020-05-17 08:37:28 -0600

    Add set up player and start menu for player prompts

--- lib/play_game_runner.rb
+++ lib/play_game_runner.rb
@@ -9,10 +9,21 @@ class PlayGameRunner
     puts "Welcome to BATTLESHIP."
     puts "Enter p to play. Enter q to quit."
 
-    input = gets.chomp
-    if input == "p"
+    computer_input = gets.chomp
+    if computer_input == "p"
       set_up_computer
     end
+
+    puts "The computers ships have been placed on the board."
+    puts "It is your turn to place your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+    puts "Enter the squares for the Cruiser (3 spaces):"
+
+    player_input = gets.chomp
+    # player needs to select 3 valid placement cells
+    # > A1 A2 A3
+    set_up_player
+    # Enter the squares for the Submarine (2 spaces):
   end
 
   def set_up_computer
@@ -44,6 +55,37 @@ class PlayGameRunner
 
     puts ""
     puts computer_board.render(true)
+  end
+
+  def set_up_player
+    player_board = Board.new
+    player_cruiser = Ship.new("Cruiser", 3)
+    player_submarine = Ship.new("Submarine", 2)
+
+    comp_cruiser_coords = Array.new
+    loop do
+      player_cruiser_coords = Array.new
+      until player_cruiser_coords.length == player_cruiser.length do
+        player_cruiser_coords << player_board.cells.keys.sample
+      end
+      break if player_board.valid_placement?(player_cruiser, player_cruiser_coords)
+    end
+
+    player_board.place(player_cruiser, player_cruiser_coords)
+
+    player_submarine_coords = Array.new
+    loop do
+      player_submarine_coords = Array.new
+      until player_submarine_coords.length == player_submarine.length do
+        player_submarine_coords << player_board.cells.keys.sample
+      end
+      break if player_board.valid_placement?(player_submarine, player_submarine_coords)
+    end
+
+    player_board.place(player_submarine, player_submarine_coords)
+
+    puts ""
+    puts player_board.render(true)
   end
 
 
