93086ce6757da4d460cedf350512acb3a29edc2c
Dani Coleman <danicoleman00@gmail.com>
2020-05-16 18:27:15 -0600

    Add start method and set up method

--- lib/play_game_runner.rb
+++ lib/play_game_runner.rb
@@ -6,9 +6,24 @@ class PlayGameRunner
 
   def start
 
-
     puts "Welcome to BATTLESHIP.
-    Enter p to play. Enter q to quit."
+  Enter p to play. Enter q to quit."
 
     input = gets.chomp
+    if input == "p"
+      set_up
+    end
   end
+
+  def set_up
+    player_board = Board.new
+    computer_board = Board.new # original_hash.to_a.sample(n).to_h
+    # generate two ships: a submarine and a cruiser for computer
+    #
+    #
+    # we want to find random cells from the @cells
+    # Check that the cells are valid
+    # if valid_placement? place_ship
+
+  end
+end
