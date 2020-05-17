6a80aa0fb22d660930983e2fd0ed1fcbb059e9a8
Dani Coleman <danicoleman00@gmail.com>
2020-05-17 08:54:42 -0600

    Add player input string to array

--- lib/play_game_runner.rb
+++ lib/play_game_runner.rb
@@ -14,16 +14,7 @@ class PlayGameRunner
       set_up_computer
     end
 
-    puts "The computers ships have been placed on the board."
-    puts "It is your turn to place your two ships."
-    puts "The Cruiser is three units long and the Submarine is two units long."
-    puts "Enter the squares for the Cruiser (3 spaces):"
-
-    player_input = gets.chomp
-    # player needs to select 3 valid placement cells
-    # > A1 A2 A3
     set_up_player
-    # Enter the squares for the Submarine (2 spaces):
   end
 
   def set_up_computer
@@ -62,31 +53,42 @@ class PlayGameRunner
     player_cruiser = Ship.new("Cruiser", 3)
     player_submarine = Ship.new("Submarine", 2)
 
-    comp_cruiser_coords = Array.new
+    puts "The computers ships have been placed on the board."
+    puts "It is your turn to place your two ships."
+    puts "The Cruiser is three units long and the Submarine is two units long."
+    puts "Enter the squares for the Cruiser (3 spaces):"
+
+
+    # player needs to select 3 valid placement cells
+    # > A1 A2 A3
+    # Enter the squares for the Submarine (2 spaces):
+
+    # player_cruiser_coords = Array.new
     loop do
-      player_cruiser_coords = Array.new
-      until player_cruiser_coords.length == player_cruiser.length do
-        player_cruiser_coords << player_board.cells.keys.sample
-      end
-      break if player_board.valid_placement?(player_cruiser, player_cruiser_coords)
+      player_input = gets.chomp
+      list = player_input.split(" ")
+      p list
+      # player_cruiser_coords = Array.new
+      # until player_cruiser_coords.length == player_cruiser.length do
+      #   player_cruiser_coords << player_board.cells.keys.sample
+      # end
+      break #player_board.valid_placement?(player_cruiser, player_cruiser_coords)
     end
 
-    player_board.place(player_cruiser, player_cruiser_coords)
-
-    player_submarine_coords = Array.new
-    loop do
-      player_submarine_coords = Array.new
-      until player_submarine_coords.length == player_submarine.length do
-        player_submarine_coords << player_board.cells.keys.sample
-      end
-      break if player_board.valid_placement?(player_submarine, player_submarine_coords)
-    end
-
-    player_board.place(player_submarine, player_submarine_coords)
-
-    puts ""
-    puts player_board.render(true)
+    # player_board.place(player_cruiser, player_cruiser_coords)
+    #
+    # # player_submarine_coords = Array.new
+    # # loop do
+    # #   # player_submarine_coords = Array.new
+    # #   # until player_submarine_coords.length == player_submarine.length do
+    # #   #   player_submarine_coords << player_board.cells.keys.sample
+    # #   # end
+    # #   break if #player_board.valid_placement?(player_submarine, player_submarine_coords)
+    # # end
+    #
+    # # player_board.place(player_submarine, player_submarine_coords)
+    #
+    # puts ""
+    # puts player_board.render(true)
   end
-
-
 end
