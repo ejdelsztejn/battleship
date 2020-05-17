b8daea0280f78609d0b993d5aae6b6eb1df58f3f
Dani Coleman <danicoleman00@gmail.com>
2020-05-17 10:30:54 -0600

    Add player chooses coordinate to fire upon

--- lib/play_game_runner.rb
+++ lib/play_game_runner.rb
@@ -84,9 +84,30 @@ class PlayGameRunner
   end
 
   def turn(computer_board, player_board)
+    # A single turn consists of:
+
+    # Displaying the boards
+    # Player choosing a coordinate to fire on
+    # Computer choosing a coordinate to fire on
+    # Reporting the result of the Player’s shot
+    # Reporting the result of the Computer’s shot
+
+    # Displaying boards
     puts "=============COMPUTER BOARD============="
     puts computer_board.render
     puts "==============PLAYER BOARD=============="
     puts player_board.render(true)
+
+    # Player shot
+    # 1.) Player prompted to give coordinate
+    # 2.) Player gives coordinate
+    # 3.) If coordinate if invalid, prompt again
+    # 4.) Else, fire on coordinate
+    loop do
+      puts "Enter coordinate to fire upon"
+      player_input = gets.chomp
+      break if computer_board.valid_coordinate?(player_input) == true
+      puts "That is not a valid coordinate. Please try again:"
+    end
   end
 end
