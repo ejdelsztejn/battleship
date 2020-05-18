2b9a6a14cbfd1108018ff878c05e16c8dd8c3f0d
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-18 16:26:06 -0400

    Add ComputerPlayer class

---
+++ lib/computer_player.rb
@@ -0,0 +1,12 @@
+class ComputerPlayer
+  attr_reader :board, :cruiser, :submarine
+  def initialize
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
+
+  def set_up_board
+    
+  end
+end
