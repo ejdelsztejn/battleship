bf6dc48dd4aeefcba93d0216f2448e8ca0b339d5
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-18 16:50:48 -0400

    Complete human player set up methods

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,30 +1,34 @@
 class HumanPlayer
-  
+  attr_reader :board, :cruiser, :submarine
+  def initialize
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+  end
+
+  def set_up_cruiser
+    loop do
+      puts "Enter the squares for the Cruiser (3 spaces):"
+      input = gets.chomp
+      cruiser_coords = input.split(" ")
+      if board.valid_placement?(cruiser, cruiser_coords) == true
+        @board.place(cruiser, cruiser_coords)
+        break
+      end
+      puts "Those are invalid coordinates. Please try again:"
+    end
+  end
+
+  def set_up_submarine
+    loop do
+      puts "Enter the squares for the Submarine (2 spaces):"
+      input = gets.chomp
+      submarine_coords = input.split(" ")
+      if board.valid_placement?(submarine, submarine_coords) == true
+        @board.place(submarine, submarine_coords)
+        break
+      end
+      puts "Those are invalid coordinates. Please try again:"
+    end
+  end
 end
-
-player_cruiser = Ship.new("Cruiser", 3)
-player_submarine = Ship.new("Submarine", 2)
-
-puts "The computers ships have been placed on the board."
-puts "It is your turn to place your two ships."
-puts "The Cruiser is three units long and the Submarine is two units long."
-
-player_cruiser_coords = Array.new
-loop do
-  puts "Enter the squares for the Cruiser (3 spaces):"
-  player_input = gets.chomp
-  player_cruiser_coords = player_input.split(" ")
-  break if player_board.valid_placement?(player_cruiser, player_cruiser_coords) == true
-  puts "Those are invalid coordinates. Please try again:"
-end
-player_board.place(player_cruiser, player_cruiser_coords)
-
-player_submarine_coords = Array.new
-loop do
-  puts "Enter the squares for the Submarine (2 spaces):"
-  player_input = gets.chomp
-  player_submarine_coords = player_input.split(" ")
-  break if player_board.valid_placement?(player_submarine, player_submarine_coords) == true
-  puts "Those are invalid coordinates. Please try again:"
-end
-player_board.place(player_submarine, player_submarine_coords)
