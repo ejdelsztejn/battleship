ed8f213718006cd5d1031c51038020dc5bdee59f
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-18 16:44:28 -0400

    Edit game class to accomodate computer and human players

--- lib/play_game_runner.rb
+++ lib/play_game_runner.rb
@@ -5,82 +5,26 @@ require './lib/board'
 class PlayGameRunner
 
   def start
-    computer_board = Board.new
-    player_board = Board.new
+    computer = ComputerPlayer.new
+    player = HumanPlayer.new
 
     puts "Welcome to BATTLESHIP."
     puts "Enter p to play. Enter q to quit."
 
-    computer_input = gets.chomp
-    if computer_input == "p"
-      set_up_computer(computer_board)
+    input = gets.chomp
+    if input == 'p'
+      computer.set_up_cruiser
+      computer.set_up_submarine
+
+      puts "The computers' ships have been placed on the board."
+      puts "It is your turn to place your two ships."
+      puts "The Cruiser is three units long and the Submarine is two units long."
+
+      player.set_up_cruiser
+      player.set_up_submarine
+
+      turn(computer, player)
     end
-
-    set_up_player(player_board)
-
-    turn(computer_board, player_board)
-  end
-
-  def set_up_computer(computer_board)
-
-    computer_cruiser = Ship.new("Cruiser", 3)
-    computer_submarine = Ship.new("Submarine", 2)
-
-    comp_cruiser_coords = Array.new
-    loop do
-      comp_cruiser_coords = Array.new
-      until comp_cruiser_coords.length == computer_cruiser.length do
-        comp_cruiser_coords << computer_board.cells.keys.sample
-      end
-      break if computer_board.valid_placement?(computer_cruiser, comp_cruiser_coords)
-    end
-
-    computer_board.place(computer_cruiser, comp_cruiser_coords)
-
-    comp_submarine_coords = Array.new
-    loop do
-      comp_submarine_coords = Array.new
-      until comp_submarine_coords.length == computer_submarine.length do
-        comp_submarine_coords << computer_board.cells.keys.sample
-      end
-      break if computer_board.valid_placement?(computer_submarine, comp_submarine_coords)
-    end
-
-    computer_board.place(computer_submarine, comp_submarine_coords)
-
-    puts ""
-    puts computer_board.render
-  end
-
-  def set_up_player(player_board)
-
-    player_cruiser = Ship.new("Cruiser", 3)
-    player_submarine = Ship.new("Submarine", 2)
-
-    puts "The computers ships have been placed on the board."
-    puts "It is your turn to place your two ships."
-    puts "The Cruiser is three units long and the Submarine is two units long."
-
-    player_cruiser_coords = Array.new
-    loop do
-      puts "Enter the squares for the Cruiser (3 spaces):"
-      player_input = gets.chomp
-      player_cruiser_coords = player_input.split(" ")
-      break if player_board.valid_placement?(player_cruiser, player_cruiser_coords) == true
-      puts "Those are invalid coordinates. Please try again:"
-    end
-    player_board.place(player_cruiser, player_cruiser_coords)
-
-    player_submarine_coords = Array.new
-    loop do
-      puts "Enter the squares for the Submarine (2 spaces):"
-      player_input = gets.chomp
-      player_submarine_coords = player_input.split(" ")
-      break if player_board.valid_placement?(player_submarine, player_submarine_coords) == true
-      puts "Those are invalid coordinates. Please try again:"
-    end
-    player_board.place(player_submarine, player_submarine_coords)
-    puts player_board.render(true)
   end
 
   def turn(computer_board, player_board)
@@ -113,7 +57,7 @@ class PlayGameRunner
       puts "That is not a valid coordinate. Please try again:"
     end
 
-    # Computer Shot 
+    # Computer Shot
     computer_input = player_board.cells.keys.sample
     if player_board.valid_coordinate?(computer_input) == true
       player_board.cells[computer_input].fire_upon
