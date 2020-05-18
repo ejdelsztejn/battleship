90789b077ad2e47aedb6a2eaad571952be0f0d4c
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-18 16:29:18 -0400

    Add set up for cruiser and submarine

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -6,7 +6,28 @@ class ComputerPlayer
     @submarine = Ship.new("Submarine", 2)
   end
 
-  def set_up_board
-    
+  def set_up_cruiser
+    loop do
+      comp_cruiser_coords = Array.new
+      until comp_cruiser_coords.length == computer_cruiser.length do
+        comp_cruiser_coords << computer_board.cells.keys.sample
+      end
+      if computer_board.valid_placement?(computer_cruiser, comp_cruiser_coords)
+        computer_board.place(computer_cruiser, comp_cruiser_coords)
+        break
+      end
+    end
+  end
+
+  def set_up_submarine
+    loop do
+      comp_submarine_coords = Array.new
+      until comp_submarine_coords.length == computer_submarine.length do
+        comp_submarine_coords << computer_board.cells.keys.sample
+      end
+      if computer_board.valid_placement?(computer_submarine, comp_submarine_coords)
+        computer_board.place(computer_submarine, comp_submarine_coords)
+      end
+    end
   end
 end
