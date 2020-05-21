ca36a21a8e4b04cecb6c7d21c96b3e2b125c6055
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-20 22:02:33 -0400

    Add small details

--- lib/game.rb
+++ lib/game.rb
@@ -20,11 +20,11 @@ class Game
     puts  " _           _   _   _           _     _        "
     puts  "| |         | | | | | |         | |   (_)       "
     puts  "| |__   __ _| |_| |_| | ___  ___| |__  _ _ __   "
-    puts  "| '_ \ / _` | __| __| |/ _ \/ __| '_ \| | '_ \  "
+    puts  "|  _ \ / _  | __| __| |/ _ \/ __|  _ \| | '_  \ "
     puts  "| |_) | (_| | |_| |_| |  __/\__ \ | | | | |_) | "
     puts  "|_.__/ \__,_|\__|\__|_|\___||___/_| |_|_| .__/  "
-    puts  "                                       | |      "
-    puts  "                                       |_|      "
+    puts  "                                        | |     "
+    puts  "                                        |_|     "
     puts  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     puts ""
     puts "Welcome to BATTLESHIP."
@@ -53,7 +53,14 @@ class Game
       puts "It is your turn to place your two ships."
       sleep(0.8)
       puts "The Cruiser is three units long and the Submarine is two units long."
+      puts ""
       sleep(0.8)
+      puts "  1 2 3 4 "
+      puts "A . . . . "
+      puts "B . . . . "
+      puts "C . . . . "
+      puts "D . . . . "
+      puts ""
 
       player.set_up_cruiser
       player.set_up_submarine
@@ -99,6 +106,12 @@ class Game
   def play_again?
     puts "Would you like to play again? (y)es / (n)o "
     input = gets.chomp
-    start if input.downcase == 'y'
+    if input.downcase == 'y'
+      @computer = ComputerPlayer.new
+      @player = HumanPlayer.new
+      @computer_ships_sunk = 0
+      @player_ships_sunk = 0
+      start
+    end
   end
 end
