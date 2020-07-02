cadac2b081027519371d9b738cb888b506d9b928
Dani Coleman <danicoleman00@gmail.com>
2020-07-02 11:00:02 -0600

    Add more to comp player test

--- test/computer_player_test.rb
+++ test/computer_player_test.rb
@@ -12,37 +12,41 @@ class ComputerPlayerTest < Minitest::Test
 
   def test_it_exists
     computer_player = ComputerPlayer.new
+    board = Board.new
 
     assert_instance_of ComputerPlayer, computer_player
   end
 
   def test_it_has_a_board
     computer_player = ComputerPlayer.new
+    board = Board.new
 
     assert_instance_of Board, computer_player.board
   end
 
   def test_it_has_ships
     computer_player = ComputerPlayer.new
+    board = Board.new
 
     assert_instance_of Ship, computer_player.cruiser
     assert_instance_of Ship, computer_player.submarine
   end
 
   def test_it_can_set_up_cruiser
-
     computer_player = ComputerPlayer.new
+    board = Board.new
     computer_player.set_up_cruiser
 
-    assert_equal [@cell, @cell, @cell], @board.place
+    assert_equal [@cell, @cell, @cell], computer_player.board.place
   end
 
   def test_it_can_set_up_submarine
     skip
     omputer_player = ComputerPlayer.new
+    board = Board.new
     computer_player.set_up_submarine
 
-    assert_equal [@cell, @cell], @board.place
+    assert_equal [@cell, @cell], computer_player.board.place
   end
 
 end
